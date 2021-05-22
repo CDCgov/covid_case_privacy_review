71c6ccb9117d229b332cc4b3fc156a100a6b3ee2
Terry Wang <dfw2@cdc.gov>
2021-05-22 17:36:41 +0000

    updated R codes on 5/22/2021

---
+++ R/geo_log_script.R
@@ -0,0 +1,24 @@
+
+cat(toString(Sys.time()))
+out_dir = "../output"
+
+log_file_name <- "geo_log_md.log"
+geo_log_file = paste(out_dir,"/",log_file_name ,sep="")
+print(geo_log_file)
+
+
+con <- file(geo_log_file)
+sink(con, append=TRUE)
+sink(con, append=TRUE, type="message")
+
+# This will echo all input and not truncate 150+ character lines...
+source("review_public_geo.R", echo=TRUE, max.deparse.length=10000)
+
+# Restore output to console
+sink() 
+sink(type="message")
+
+# And look at the log...
+cat(readLines("geo_log_md.log"), sep="\n")
+
+cat(toString(Sys.time()))
---
+++ R/pud_12_log_script.R
@@ -0,0 +1,25 @@
+
+cat(toString(Sys.time()))
+
+out_dir = "../output"
+
+log_file_name <- "pud_12_log_md.log"
+pud_log_file = paste(out_dir,"/",log_file_name ,sep="")
+print(pud_log_file)
+
+
+con <- file(pud_log_file)
+sink(con, append=TRUE)
+sink(con, append=TRUE, type="message")
+
+# This will echo all input and not truncate 150+ character lines...
+source("review_public.R", echo=TRUE, max.deparse.length=10000)
+
+# Restore output to console
+sink() 
+sink(type="message")
+
+# And look at the log...
+cat(readLines(pud_log_file), sep="\n")
+
+cat(toString(Sys.time()))
